547adff6447fd4ecd75cc7409c944366a5d5f599
nooh <nooh64@live.com>
2019-04-06 17:03:08 +0530

    added mongo

--- app.js
+++ app.js
@@ -9,18 +9,13 @@ var url = process.env.MONGODB_URI || "mongodb://localhost:27017/mydb";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
-  dbo.collection("todos").insertOne({name:"Sample"}, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-  //db.close();
+  db.close();
 });
 
 console.log("Starting server on port ",port);
 
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
+  res.write('Hello World!'+url); //write a response to the client
   res.end(); //end the response
 }).listen(port);
 
